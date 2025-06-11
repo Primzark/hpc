cba974ac729521c1b266fcf9471a9c6a423cc678
Primzark <starlod7696@gmail.com>
2025-06-11 12:56:21 +0200

    Use shared PDO helper

--- src/Model/model-evenement.php
+++ src/Model/model-evenement.php
@@ -1,11 +1,19 @@
 <?php
 class Evenement
 {
-
-    public static function ajouter($titre, $lieu, $date, $heure, $description, $image, $id_type_eve)
+    /**
+     * Returns a configured PDO instance.
+     */
+    private static function getPDO()
     {
         $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+
+    public static function ajouter($titre, $lieu, $date, $heure, $description, $image, $id_type_eve)
+    {
+        $pdo = self::getPDO();
 
         $sql = "INSERT INTO evenement (eve_titre, eve_lieu, eve_date, eve_heure, eve_description, eve_image, id_type_eve)
                 VALUES (:titre, :lieu, :date, :heure, :description, :image, :id_type_eve)";
@@ -18,12 +26,13 @@ class Evenement
         $stmt->bindValue(':image', $image);
         $stmt->bindValue(':id_type_eve', $id_type_eve);
         $stmt->execute();
+
+        return $pdo->lastInsertId();
     }
 
     public static function getAll()
     {
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = self::getPDO();
 
         $sql = "SELECT * FROM evenement ORDER BY eve_date DESC";
         $stmt = $pdo->query($sql);
@@ -32,8 +41,7 @@ class Evenement
 
     public static function getById($id_eve)
     {
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = self::getPDO();
 
         $sql = "SELECT * FROM evenement WHERE id_eve = :id";
         $stmt = $pdo->prepare($sql);
@@ -44,8 +52,7 @@ class Evenement
 
     public static function updateEvenement($id_eve, $data)
     {
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = self::getPDO();
 
         $sql = "UPDATE evenement 
             SET eve_lieu = :lieu, eve_date = :date, eve_heure = :heure, eve_description = :description, eve_image = :image 
