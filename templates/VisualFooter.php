e68abff9097f2c4ee285a3d822444136738c93d5
Primzark <starlod7696@gmail.com>
2025-06-11 17:39:17 +0200

    add

--- templates/VisualFooter.php
+++ templates/VisualFooter.php
@@ -1,28 +1,31 @@
-<?php if (!empty($showPartners)) : ?>
+<?php if (!empty($showPartners)): ?>
     <div class="py-4">
         <div class="container">
             <p class="text-center text-warning fw-semibold mb-3">Nos partenaires</p>
             <div class="row justify-content-center g-3">
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/Logo-POKERSTARS.png" alt="PokerStars" class="img-fluid partner-logo">
+                    <img src="/asset/img/Logo-POKERSTARS.png" alt="PokerStars" class="img-fluid object-fit-contain">
                 </div>
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/namur_casino.png" alt="Circus Casino Namur" class="img-fluid partner-logo">
+                    <img src="/asset/img/namur_casino.png" alt="Circus Casino Namur"
+                        class="img-fluid object-fit-contain">
                 </div>
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/logo-pasino-havre.png" alt="Pasino Le Havre" class="img-fluid partner-logo">
+                    <img src="/asset/img/logo-pasino-havre.png" alt="Pasino Le Havre"
+                        class="img-fluid object-fit-contain">
                 </div>
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/confo.png" alt="Conforama" class="img-fluid partner-logo">
+                    <img src="/asset/img/confo.png" alt="Conforama" class="img-fluid object-fit-contain">
                 </div>
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/auto-ecole.jpeg" alt="Auto-école d’Ingouville" class="img-fluid partner-logo">
+                    <img src="/asset/img/auto-ecole.jpeg" alt="Auto-école d’Ingouville"
+                        class="img-fluid object-fit-contain">
                 </div>
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/caval_brod.jpg" alt="Caval Brod" class="img-fluid partner-logo">
+                    <img src="/asset/img/caval_brod.jpg" alt="Caval Brod" class="img-fluid object-fit-contain">
                 </div>
                 <div class="col-4 col-sm-3 col-md-2">
-                    <img src="/asset/img/dfds.png" alt="DFDS" class="img-fluid partner-logo">
+                    <img src="/asset/img/dfds.png" alt="DFDS" class="img-fluid object-fit-contain">
                 </div>
             </div>
         </div>
@@ -37,12 +40,10 @@
                     propos</a>
             </li>
             <li class="list-inline-item">
-                <a href="/src/Controller/ContactController.php"
-                    class="text-decoration-none login-hint">Contact</a>
+                <a href="/src/Controller/ContactController.php" class="text-decoration-none login-hint">Contact</a>
             </li>
             <li class="list-inline-item">
-                <a href="/src/Controller/RegleController.php"
-                    class="text-decoration-none login-hint">Règles</a>
+                <a href="/src/Controller/RegleController.php" class="text-decoration-none login-hint">Règles</a>
             </li>
         </ul>
     </div>
