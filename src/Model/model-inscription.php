e2bc9659828e2194c2b22406ba6ffa3a7377b18f
Primzark <starlod7696@gmail.com>
2025-06-11 13:05:52 +0200

    Share PDO helper in user and registration models

--- src/Model/model-inscription.php
+++ src/Model/model-inscription.php
@@ -1,9 +1,16 @@
 <?php
 class Inscription {
-
-    public static function dejaInscrit($id_eve, $id_uti) {
+    /**
+     * Get a PDO connection for database operations.
+     */
+    private static function getPDO() {
         $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+
+    public static function dejaInscrit($id_eve, $id_uti) {
+        $pdo = self::getPDO();
 
         $sql = "SELECT * FROM s_inscrit_a WHERE id_eve = :id_eve AND id_uti = :id_uti";
         $stmt = $pdo->prepare($sql);
@@ -13,8 +20,7 @@ class Inscription {
         return $stmt->fetch() !== false;
     }
     public static function inscrire($id_eve, $id_uti) {
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = self::getPDO();
 
         $sql = "INSERT INTO s_inscrit_a (id_uti, id_eve) VALUES (:id_uti, :id_eve)";
         $stmt = $pdo->prepare($sql);
@@ -24,8 +30,7 @@ class Inscription {
     }
 
     public static function desinscrire($id_eve, $id_uti) {
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = self::getPDO();
 
         $sql = "DELETE FROM s_inscrit_a WHERE id_uti = :id_uti AND id_eve = :id_eve";
         $stmt = $pdo->prepare($sql);
