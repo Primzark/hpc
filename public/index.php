19a6469aa34225ee6cb8657f9d1e87cf6fe144cb
Primzark <starlod7696@gmail.com>
2025-06-13 11:31:58 +0200

    Affiche un message quand il n'y a pas de contenu

--- public/index.php
+++ public/index.php
@@ -54,23 +54,29 @@
         </div>
 
         <div class="row g-3 mb-5">
-            <?php foreach ($tournois as $tournoi): ?>
-                <div class="col-12 col-md-4">
-                    <div class="p-2 custom-bg rounded h-100">
-                        <div class="ratio ratio-16x9 mb-2 rounded overflow-hidden">
-                            <img src="/asset/img/<?= htmlspecialchars($tournoi['eve_image']) ?>"
-                                class="w-100 h-100 object-fit-cover" alt="Image du tournoi">
-                        </div>
-                        <p class="text-light fw-semibold small mb-1"><?= htmlspecialchars($tournoi['eve_titre']) ?></p>
-                        <p class="custom-text small mb-2">
-                            <?= date('d/m/Y', strtotime($tournoi['eve_date'])) ?> -
-                            <?= htmlspecialchars($tournoi['eve_lieu']) ?>
-                        </p>
-                        <a href="/src/Controller/PageEvenementController.php?id=<?= $tournoi['id_eve'] ?>"
-                            class="btn btn-sm btn-outline-warning w-100">Détails</a>
-                    </div>
+            <?php if (empty($tournois)): ?>
+                <div class="col-12">
+                    <p class="text-warning text-center">Aucun tournoi à venir pour le moment.</p>
                 </div>
-            <?php endforeach; ?>
+            <?php else: ?>
+                <?php foreach ($tournois as $tournoi): ?>
+                    <div class="col-12 col-md-4">
+                        <div class="p-2 custom-bg rounded h-100">
+                            <div class="ratio ratio-16x9 mb-2 rounded overflow-hidden">
+                                <img src="/asset/img/<?= htmlspecialchars($tournoi['eve_image']) ?>"
+                                    class="w-100 h-100 object-fit-cover" alt="Image du tournoi">
+                            </div>
+                            <p class="text-light fw-semibold small mb-1"><?= htmlspecialchars($tournoi['eve_titre']) ?></p>
+                            <p class="custom-text small mb-2">
+                                <?= date('d/m/Y', strtotime($tournoi['eve_date'])) ?> -
+                                <?= htmlspecialchars($tournoi['eve_lieu']) ?>
+                            </p>
+                            <a href="/src/Controller/PageEvenementController.php?id=<?= $tournoi['id_eve'] ?>"
+                                class="btn btn-sm btn-outline-warning w-100">Détails</a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
 
         <!-- News Section -->
@@ -81,23 +87,29 @@
         </div>
 
         <div class="row g-3">
-            <?php foreach ($actus as $actu): ?>
-                <div class="col-12 col-md-4">
-                    <div class="p-2 custom-bg rounded h-100">
-                        <div class="ratio ratio-16x9 mb-2 rounded overflow-hidden">
-                            <img src="/asset/img/<?= htmlspecialchars($actu['eve_image']) ?>"
-                                class="w-100 h-100 object-fit-cover" alt="Image de l’actualité">
-                        </div>
-                        <p class="text-light fw-semibold small mb-1"><?= htmlspecialchars($actu['eve_titre']) ?></p>
-                        <p class="custom-text small mb-2">
-                            <?= date('d/m/Y', strtotime($actu['eve_date'])) ?> –
-                            <?= substr(htmlspecialchars($actu['eve_description']), 0, 20) ?>…
-                        </p>
-                        <a href="/src/Controller/PageEvenementController.php?id=<?= $actu['id_eve'] ?>"
-                            class="btn btn-sm btn-outline-warning w-100">Lire</a>
-                    </div>
+            <?php if (empty($actus)): ?>
+                <div class="col-12">
+                    <p class="text-warning text-center">Aucune actualité pour le moment.</p>
                 </div>
-            <?php endforeach; ?>
+            <?php else: ?>
+                <?php foreach ($actus as $actu): ?>
+                    <div class="col-12 col-md-4">
+                        <div class="p-2 custom-bg rounded h-100">
+                            <div class="ratio ratio-16x9 mb-2 rounded overflow-hidden">
+                                <img src="/asset/img/<?= htmlspecialchars($actu['eve_image']) ?>"
+                                    class="w-100 h-100 object-fit-cover" alt="Image de l’actualité">
+                            </div>
+                            <p class="text-light fw-semibold small mb-1"><?= htmlspecialchars($actu['eve_titre']) ?></p>
+                            <p class="custom-text small mb-2">
+                                <?= date('d/m/Y', strtotime($actu['eve_date'])) ?> –
+                                <?= substr(htmlspecialchars($actu['eve_description']), 0, 20) ?>…
+                            </p>
+                            <a href="/src/Controller/PageEvenementController.php?id=<?= $actu['id_eve'] ?>"
+                                class="btn btn-sm btn-outline-warning w-100">Lire</a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 
