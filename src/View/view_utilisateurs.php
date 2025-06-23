41e5d631e63dfba66b2679c02921d5eae0516d13
Primzark <starlod7696@gmail.com>
2025-06-23 19:22:21 +0200

    Allow deleting members with confirmation

--- src/View/view_utilisateurs.php
+++ src/View/view_utilisateurs.php
@@ -14,14 +14,51 @@
         <?php else: ?>
             <ul class="list-group">
                 <?php foreach ($utilisateurs as $utilisateur): ?>
-                    <li class="list-group-item form-section-bg text-light">
+                    <li class="list-group-item form-section-bg text-light d-flex justify-content-between align-items-center">
                         <?php echo htmlspecialchars($utilisateur['uti_nom']); ?>
+                        <?php if (isset($_SESSION['user_id'])): ?>
+                            <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" data-user-id="<?php echo $utilisateur['id_uti']; ?>">Supprimer</button>
+                        <?php endif; ?>
                     </li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
 
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <!-- Modal de confirmation -->
+            <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content custom-bg text-light">
+                        <div class="modal-header border-0">
+                            <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmer la suppression</h5>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Fermer"></button>
+                        </div>
+                        <div class="modal-body">
+                            Êtes-vous sûr de vouloir supprimer ce membre ?
+                        </div>
+                        <div class="modal-footer border-0">
+                            <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Annuler</button>
+                            <a href="#" class="btn custom-add text-light" id="confirmDeleteBtn">Supprimer</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
+
     </div>
     <?php include_once('../../templates/VisualFooter.php'); ?>
     <?php include_once('../../templates/footer.php'); ?>
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <script>
+            const deleteModal = document.getElementById('confirmDeleteModal');
+            if (deleteModal) {
+                deleteModal.addEventListener('show.bs.modal', function (event) {
+                    const button = event.relatedTarget;
+                    const userId = button.getAttribute('data-user-id');
+                    const confirmBtn = document.getElementById('confirmDeleteBtn');
+                    confirmBtn.href = '/src/Controller/SupprimerUtilisateurController.php?id=' + userId;
+                });
+            }
+        </script>
+    <?php endif; ?>
 </div>
