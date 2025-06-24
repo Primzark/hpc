b13a7745cb9b26d0d3218a47bcd04c1c9dae8976
Primzark <starlod7696@gmail.com>
2025-06-24 23:47:55 +0200

    Configure PHPMailer to use SMTP

--- config.php
+++ config.php
@@ -9,6 +9,12 @@ define('DB_PASS', 'wC7t4O3s4c3ytIpPYkIA');
 define('RECAPTCHA_SITE_KEY', '6LfHSWsrAAAAAM2CWcvUV2W4psnR2B7Ct8cP9rhE');
 define('RECAPTCHA_SECRET_KEY', '6LfHSWsrAAAAAFrZ9oXAepgD4FnTnfitEIBo-vzN');
 
+// SMTP configuration for PHPMailer
+define('SMTP_HOST', 'smtp.example.com');
+define('SMTP_PORT', 587);
+define('SMTP_USER', 'username@example.com');
+define('SMTP_PASS', 'yourpassword');
+
 // Base path for building absolute URLs
 $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
 define('BASE_PATH', $base === '/' ? '' : $base);
