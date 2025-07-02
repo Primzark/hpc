3c7b9d4ee7af14f519b73136d8662e811a51f560
Primzark <starlod7696@gmail.com>
2025-07-02 15:38:19 +0200

    add

--- src/Controller/admin_required.php
+++ src/Controller/admin_required.php
@@ -1,7 +1,6 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
+
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_admin']) || $_SESSION['user_admin'] != 1) {
     header('Location: /');
     exit;
