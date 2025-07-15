f92f7391a77ee5383b20a6d55883f641b96f5b16
Primzark <starlod7696@gmail.com>
2025-07-16 01:31:49 +0200

    Add admin deletion of ranking entries

--- src/View/view_classement.php
+++ src/View/view_classement.php
@@ -23,20 +23,62 @@ include_once(__DIR__ . '/../../templates/navbar.php');
             <div class="row text-light fw-bold border-bottom py-2">
                 <div class="col-4 col-md-1">Rang</div>
                 <div class="col-4 col-md-7">Joueur</div>
-                <div class="col-4 col-md-4">Points</div>
+                <div class="col-4 col-md-3">Points</div>
+                <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
+                    <div class="d-none d-md-block col-md-1">Action</div>
+                <?php endif; ?>
             </div>
 
             <!-- Lignes de données -->
-            <?php foreach ($classement as $entry): ?>
+        <?php foreach ($classement as $entry): ?>
                 <div class="row border-bottom py-2 custom-text align-items-center">
                     <div class="col-4 col-md-1"><?php echo htmlspecialchars($entry['cla_rang']); ?></div>
                     <div class="col-4 col-md-7"><?php echo htmlspecialchars($entry['cla_nomjoueur']); ?></div>
-                    <div class="col-4 col-md-4"><?php echo htmlspecialchars($entry['cla_points']); ?></div>
+                    <div class="col-4 col-md-3"><?php echo htmlspecialchars($entry['cla_points']); ?></div>
+                    <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
+                        <div class="col-12 col-md-1 text-md-end mt-2 mt-md-0">
+                            <button class="btn btn-sm custom-add text-light" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" data-gen-id="<?php echo $entry['cla_id_gen']; ?>">Supprimer</button>
+                        </div>
+                    <?php endif; ?>
                 </div>
-            <?php endforeach; ?>
+        <?php endforeach; ?>
         </div>
+
+        <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
+            <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content custom-bg text-light">
+                        <div class="modal-header border-0">
+                            <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmer la suppression</h5>
+                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Fermer"></button>
+                        </div>
+                        <div class="modal-body">
+                            Êtes-vous sûr de vouloir supprimer ce joueur du classement ?
+                        </div>
+                        <div class="modal-footer border-0">
+                            <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Annuler</button>
+                            <a href="#" class="btn custom-add text-light" id="confirmDeleteBtn">Supprimer</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
 
         <?php include_once(__DIR__ . '/../../templates/VisualFooter.php'); ?>
         <?php include_once(__DIR__ . '/../../templates/footer.php'); ?>
     </div>
 </div>
+
+<?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
+    <script>
+        const deleteModal = document.getElementById('confirmDeleteModal');
+        if (deleteModal) {
+            deleteModal.addEventListener('show.bs.modal', function (event) {
+                const button = event.relatedTarget;
+                const genId = button.getAttribute('data-gen-id');
+                const confirmBtn = document.getElementById('confirmDeleteBtn');
+                confirmBtn.href = '/supprimer-classement?id=' + genId;
+            });
+        }
+    </script>
+<?php endif; ?>
