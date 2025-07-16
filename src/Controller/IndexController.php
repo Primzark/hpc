c8fd50c32342ae0b7f2981a6900abba5e1fd50a1
Primzark <starlod7696@gmail.com>
2025-07-16 23:51:29 +0200

    add

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,11 +3,9 @@ session_start();
 require_once __DIR__ . '/../../config.php';
 require_once __DIR__ . '/../Model/model-evenement.php';
 require_once __DIR__ . '/../Model/model-utilisateur.php';
-require_once __DIR__ . '/../Model/model-inscription.php';
 
 $tournois = Evenement::getLatestByType(2, 3);
 $actus = Evenement::getLatestByType(1, 3);
-$userTournois = [];
 
 $utiNom = '';
 if (isset($_SESSION['user_id'])) {
@@ -15,7 +13,6 @@ if (isset($_SESSION['user_id'])) {
     if ($user) {
         $utiNom = $user['uti_nom'];
     }
-    $userTournois = Inscription::getUpcomingByUser($_SESSION['user_id']);
 }
 
 // Chargement de l'index
