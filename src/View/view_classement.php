16118f62d4a03ef48cc38d4d6f2518bd384ce8b2
Primzark <starlod7696@gmail.com>
2025-07-16 23:11:01 +0200

    Remove classement deletion feature

--- src/View/view_classement.php
+++ src/View/view_classement.php
@@ -24,9 +24,7 @@ include_once(__DIR__ . '/../../templates/navbar.php');
                 <div class="col-4 col-md-1">Rang</div>
                 <div class="col-4 col-md-7">Joueur</div>
                 <div class="col-4 col-md-3">Points</div>
-                <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
-                    <div class="d-none d-md-block col-md-1">Action</div>
-                <?php endif; ?>
+                <?php // Action column removed ?>
             </div>
 
             <!-- Lignes de données -->
@@ -35,34 +33,12 @@ include_once(__DIR__ . '/../../templates/navbar.php');
                     <div class="col-4 col-md-1"><?php echo htmlspecialchars($entry['cla_rang']); ?></div>
                     <div class="col-4 col-md-7"><?php echo htmlspecialchars($entry['cla_nomjoueur']); ?></div>
                     <div class="col-4 col-md-3"><?php echo htmlspecialchars($entry['cla_points']); ?></div>
-                    <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
-                        <div class="col-12 col-md-1 text-md-end mt-2 mt-md-0">
-                            <button class="btn btn-sm custom-add text-light" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" data-gen-id="<?php echo $entry['cla_id_gen']; ?>">Supprimer</button>
-                        </div>
-                    <?php endif; ?>
+                    <?php // Delete button removed ?>
                 </div>
         <?php endforeach; ?>
         </div>
 
-        <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
-            <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content custom-bg text-light">
-                        <div class="modal-header border-0">
-                            <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmer la suppression</h5>
-                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Fermer"></button>
-                        </div>
-                        <div class="modal-body">
-                            Êtes-vous sûr de vouloir supprimer ce joueur du classement ?
-                        </div>
-                        <div class="modal-footer border-0">
-                            <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Annuler</button>
-                            <a href="#" class="btn custom-add text-light" id="confirmDeleteBtn">Supprimer</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endif; ?>
+        <?php // Delete confirmation modal removed ?>
 
         <?php include_once(__DIR__ . '/../../templates/VisualFooter.php'); ?>
         <?php include_once(__DIR__ . '/../../templates/footer.php'); ?>
@@ -70,15 +46,5 @@ include_once(__DIR__ . '/../../templates/navbar.php');
 </div>
 
 <?php if (isset($_SESSION['user_id']) && isset($_SESSION['user_admin']) && $_SESSION['user_admin'] == 1): ?>
-    <script>
-        const deleteModal = document.getElementById('confirmDeleteModal');
-        if (deleteModal) {
-            deleteModal.addEventListener('show.bs.modal', function (event) {
-                const button = event.relatedTarget;
-                const genId = button.getAttribute('data-gen-id');
-                const confirmBtn = document.getElementById('confirmDeleteBtn');
-                confirmBtn.href = '/supprimer-classement?id=' + genId;
-            });
-        }
-    </script>
+    <!-- Delete script removed -->
 <?php endif; ?>
